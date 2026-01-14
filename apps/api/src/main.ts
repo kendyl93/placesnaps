77bf844434eb064a7664dc27f8e98a05da6780f3
Pawel Stanecki <pstanecki@usertesting.com>
2026-01-14 21:39:51 +0100

    Refactor code structure for improved readability and maintainability

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000'],
-  });
+  app.enableCors({ origin: true }); // temporary
 
   await app.listen(process.env.PORT ?? 3001);
 }
